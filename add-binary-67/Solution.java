ac88cb0303865bf2c610b3746fa062a7ecc66dd2
Stela Palii <stelasimonapalii@gmail.com>
2023-02-13 17:05:55 -0800

    LeetCode problem # 67, 'Add Binary'

---
+++ add-binary-67/Solution.java
@@ -0,0 +1,28 @@
+/*
+ * LeetCode problem # 67, 'Add Binary'
+ * https://leetcode.com/problems/add-binary/
+ * 
+ * Given two binary strings a and b, return their sum as a binary string.
+ * 
+ */
+
+
+import java.math.BigInteger;
+
+class Solution {
+  public String addBinary(String a, String b) {
+    BigInteger x = new BigInteger(a, 2);
+    BigInteger y = new BigInteger(b, 2);
+    BigInteger zero = new BigInteger("0", 2);
+    BigInteger carry;
+    BigInteger res;
+
+    while (y.compareTo(zero) != 0) {
+      res = x.xor(y);
+      carry = x.and(y).shiftLeft(1);
+      x = res;
+      y = carry;
+    }
+    return x.toString(2);
+  }
+}
