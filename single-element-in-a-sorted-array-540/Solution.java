ae5819961f9aea6921cdd48c18677cad40b02a36
Stela Palii <stelasimonapalii@gmail.com>
2023-02-20 17:08:54 -0800

    LeetCode problem # 540, 'Single Element in a Sorted Array'

---
+++ single-element-in-a-sorted-array-540/Solution.java
@@ -0,0 +1,26 @@
+/*
+ * LeetCode problem # 540, 'Single Element in a Sorted Array'
+ * https://leetcode.com/problems/single-element-in-a-sorted-array/
+ *
+ */
+
+class Solution {
+    public int singleNonDuplicate(int[] nums) {
+        int low = 0;
+        int high = nums.length - 1;
+
+        while (low < high) {
+            int mid = low + (high - low) / 2;
+
+            if (mid % 2 == 1) {
+                --mid;
+            }
+            if (nums[mid] == nums[mid + 1]) {
+                low = mid + 2;
+            } else {
+                high = mid;
+            }
+        }
+        return nums[low];
+    }
+}
