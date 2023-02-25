5b62189bab2a165dc783901ef522e9591700fb38
Stela Palii <stelasimonapalii@gmail.com>
2023-02-24 17:03:54 -0800

    LeetCode problem # 121, 'Best Time to Buy and Sell Stock'

---
+++ best-time-to-buy-and-sell-stock-121/Solution.java
@@ -0,0 +1,18 @@
+/*
+ * LeetCode problem # 121, 'Best Time to Buy and Sell Stock'
+ * https://leetcode.com/problems/best-time-to-buy-and-sell-stock/
+ */
+
+class Solution {
+    public int maxProfit(int[] prices) {
+        int minPrice = Integer.MAX_VALUE;
+        int maxProfit = Integer.MIN_VALUE;
+
+        for (int price : prices) {
+            int potentialProfit = price - minPrice;
+            maxProfit = Math.max(maxProfit, potentialProfit);
+            minPrice = Math.min(minPrice, price);
+        }
+        return maxProfit < 0 ? 0 : maxProfit;
+    }
+}
