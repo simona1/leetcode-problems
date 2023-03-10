f5ea9a9481e81e66d35c55661469ea4ddabde389
Stela Palii <stelasimonapalii@gmail.com>
2023-03-09 17:56:39 -0800

    LeetCode problem # 382, 'Linked List Random Node'

---
+++ linked-list-random-node-382/Solution.java
@@ -0,0 +1,24 @@
+/*
+ * LeetCode problem # 382, 'Linked List Random Node'
+ * https://leetcode.com/problems/linked-list-random-node/
+ */
+
+import java.util.*;
+
+class Solution {
+    private List<Integer> values = new ArrayList<>();
+
+    public Solution(ListNode head) {
+        ListNode curr = head;
+        while (curr != null) {
+            values.add(curr.val);
+            curr = curr.next;
+        }
+    }
+
+    public int getRandom() {
+        int rand = (int) (Math.random() * this.values.size());
+        return this.values.get(rand);
+
+    }
+}
