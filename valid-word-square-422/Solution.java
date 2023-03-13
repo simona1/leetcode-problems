5adb65902014bd6694ff371b908dbf461f874a67
Stela Palii <stelasimonapalii@gmail.com>
2023-03-12 17:28:15 -0700

    LeetCode problem # 422, 'Valid Word Square'

---
+++ valid-word-square-422/Solution.java
@@ -0,0 +1,31 @@
+/*
+ * LeetCode problem # 422, 'Valid Word Square'
+ * https://leetcode.com/problems/valid-word-square
+ */
+
+import java.util.*;
+
+class Solution {
+    public boolean validWordSquare(List<String> words) {
+        for (int i = 0; i < words.size(); ++i) {
+            String row = words.get(i);
+            String col = readCol(words, i);
+            if (!row.equals(col)) {
+                return false;
+            }
+        }
+        return true;
+    }
+
+    private String readCol(List<String> words, int index) {
+        StringBuilder sb = new StringBuilder();
+
+        for (String word : words) {
+            if (index >= word.length()) {
+                return sb.toString();
+            }
+            sb.append(word.charAt(index));
+        }
+        return sb.toString();
+    }
+}
