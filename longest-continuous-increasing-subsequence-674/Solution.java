afab945113ab89857657a92212bca0eace87397c
Stela Palii <stelasimonapalii@gmail.com>
2023-03-14 08:56:27 -0700

    LeetCode problem # 674, 'Longest Continuous Increasing Subsequence'

---
+++ longest-continuous-increasing-subsequence-674/Solution.java
@@ -0,0 +1,19 @@
+/*
+ * LeetCode problem # 674, 'Longest Continuous Increasing Subsequence'
+ * https://leetcode.com/problems/longest-continuous-increasing-subsequence/
+ */
+
+class Solution {
+    public int findLengthOfLCIS(int[] nums) {
+        int res = 0;
+        int ptr = 0;
+
+        for (int i = 0; i < nums.length; ++i) {
+            if (i > 0 && nums[i - 1] >= nums[i]) {
+                ptr = i;
+            }
+            res = Math.max(res, i - ptr + 1);
+        }
+        return res;
+    }
+}
