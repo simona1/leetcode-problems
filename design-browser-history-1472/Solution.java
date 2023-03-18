2cfde57e2814fdc247e88fda33e2c350d18c0b16
Stela Palii <stelasimonapalii@gmail.com>
2023-03-17 19:19:17 -0700

    LeetCode problem # 1472, 'Design Browser History'

---
+++ design-browser-history-1472/Solution.java
@@ -0,0 +1,41 @@
+
+/**
+* LeetCode problem # 1472, 'Design Browser History'
+* https://leetcode.com/problems/design-browser-history
+*/
+
+import java.util.*;
+
+class BrowserHistory {
+    private Stack<String> bwd;
+    private Stack<String> fwd;
+    private String curr;
+
+    public BrowserHistory(String homepage) {
+        this.bwd = new Stack<>();
+        this.fwd = new Stack<>();
+        this.curr = homepage;
+    }
+
+    public void visit(String url) {
+        this.bwd.push(this.curr);
+        this.curr = url;
+        this.fwd = new Stack<>();
+    }
+
+    public String back(int steps) {
+        while (steps-- > 0 && !this.bwd.isEmpty()) {
+            this.fwd.push(this.curr);
+            this.curr = this.bwd.pop();
+        }
+        return this.curr;
+    }
+
+    public String forward(int steps) {
+        while (steps-- > 0 && !this.fwd.isEmpty()) {
+            this.bwd.push(this.curr);
+            this.curr = this.fwd.pop();
+        }
+        return this.curr;
+    }
+}
