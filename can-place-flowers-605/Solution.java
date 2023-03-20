9e12182277bb5348c92f032eb65fd657f14e0e2f
Stela Palii <stelasimonapalii@gmail.com>
2023-03-19 17:28:31 -0700

    LeetCode problem # 605, 'Can Place Flowers'

---
+++ can-place-flowers-605/Solution.java
@@ -0,0 +1,21 @@
+/*
+* LeetCode problem # 605, 'Can Place Flowers'
+* https://leetcode.com/problems/can-place-flowers/
+*/
+
+class Solution {
+    public boolean canPlaceFlowers(int[] flowerbed, int n) {
+        int count = 0;
+        for (int i = 0; i < flowerbed.length; ++i) {
+            if (flowerbed[i] == 0) {
+                boolean left = i == 0 || flowerbed[i - 1] == 0;
+                boolean right = i == flowerbed.length - 1 || flowerbed[i + 1] == 0;
+                if (left && right) {
+                    flowerbed[i] = 1;
+                    ++count;
+                }
+            }
+        }
+        return count >= n;
+    }
+}
