0513d2a579802c50b398d939aa2dc33fea855c71
Stela Palii <stelasimonapalii@gmail.com>
2023-03-31 21:06:37 -0700

    Update to LeetCode problem # 2602

--- minimum-operations-to-make-all-array-elements-equal-2602/Solution.java
+++ minimum-operations-to-make-all-array-elements-equal-2602/Solution.java
@@ -7,38 +7,39 @@
 import java.util.*;
 
 class Solution {
+
+    private int binarySearch(int[] nums, int value) {
+        int lo = -1;
+        int hi = nums.length;
+
+        while (lo + 1 != hi) {
+            int mid = lo + (hi - lo) / 2;
+            if (value >= nums[mid]) {
+                lo = mid;
+            } else {
+                hi = mid;
+            }
+        }
+        return hi;
+    }
+
     public List<Long> minOperations(int[] nums, int[] queries) {
         List<Long> res = new ArrayList<>();
 
         Arrays.sort(nums);
         int n = nums.length;
 
-        long[] prefixSum = new long[n + 1];
-
+        long[] prefixSums = new long[n + 1];
         for (int i = 0; i < n; ++i) {
-            prefixSum[i + 1] = prefixSum[i] + nums[i];
+            prefixSums[i + 1] = prefixSums[i] + nums[i];
         }
 
         for (int q : queries) {
             int index = binarySearch(nums, q);
-            long left = 1L * q * index - prefixSum[index] + prefixSum[n] - prefixSum[index];
-            long right = 1L * (n - index) * q;
-            res.add(left - right);
+            long sum1 = 1L * q * index - prefixSums[index];
+            long sum2 = prefixSums[n] - prefixSums[index] - (1L * (n - index) * q);
+            res.add(sum1 + sum2);
         }
         return res;
-    }
-
-    private int binarySearch(int[] nums, int target) {
-        int lo = 0;
-        int hi = nums.length;
-        while (lo < hi) {
-            int mid = lo + (hi - lo) / 2;
-            if (target > nums[mid]) {
-                lo = mid + 1;
-            } else {
-                hi = mid;
-            }
-        }
-        return lo;
     }
 }
