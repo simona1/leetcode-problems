f4811a0de8508982e6ccfb77670128856e810d7e
Stela Palii <stelasimonapalii@gmail.com>
2023-05-05 21:30:22 -0700

    LeetCode problem # 2549, 'Count Distinct Numbers on Board'

---
+++ count-distinct-numbers-on-board-2549/Solution.java
@@ -0,0 +1,11 @@
+/**
+ * LeetCode problem # 2549, 'Count Distinct Numbers on Board'
+ * https://leetcode.com/problems/count-distinct-numbers-on-board
+ */
+
+class Solution {
+    public int distinctIntegers(int n) {
+        return (n == 1) ? n : n - 1;
+
+    }
+}
