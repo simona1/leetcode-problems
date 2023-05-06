e8c6acc396b1b2f38300ffc44255ce09c0740afa
Stela Palii <stelasimonapalii@gmail.com>
2023-05-05 21:19:54 -0700

    LeetCode problem # 836, 'Rectangle Overlap'

---
+++ rectangle-overlap-836/Solution.java
@@ -0,0 +1,12 @@
+
+/**
+ * LeetCode problem # 836, 'Rectangle Overlap'
+ * https://leetcode.com/problems/rectangle-overlap
+ */
+
+class Solution {
+    public boolean isRectangleOverlap(int[] rec1, int[] rec2) {
+        return (Math.min(rec1[2], rec2[2]) > Math.max(rec1[0], rec2[0])
+                && Math.min(rec1[3], rec2[3]) > Math.max(rec1[1], rec2[1]));
+    }
+}
