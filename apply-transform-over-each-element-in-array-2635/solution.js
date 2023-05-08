b3a5e1f2d9f91413481e473867c268df68a8c63d
Stela Palii <stelasimonapalii@gmail.com>
2023-05-07 17:07:12 -0700

    LeetCode problem # 2635, 'Apply Transform Over Each Element in Array'

---
+++ apply-transform-over-each-element-in-array-2635/solution.js
@@ -0,0 +1,18 @@
+/**
+ * LeetCode problem # 2635, 'Apply Transform Over Each Element in Array'
+ * https://leetcode.com/problems/apply-transform-over-each-element-in-array
+ */
+
+/**
+ * @param {number[]} arr
+ * @param {Function} fn
+ * @return {number[]}
+ */
+var map = function (arr, fn) {
+  const res = [];
+
+  for (let i = 0; i < arr.length; ++i) {
+    res[i] = fn(arr[i], i);
+  }
+  return res;
+};
