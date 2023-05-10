f090e354551095150b4dd0a73253d4cd0dc84e51
Stela Palii <stelasimonapalii@gmail.com>
2023-05-09 21:51:39 -0700

    LeetCode problem # 2626, 'Array Reduce Transformation'

---
+++ array-reduce-transformation-2626/solution.js
@@ -0,0 +1,18 @@
+/**
+ * LeetCode problem # 2626, 'Array Reduce Transformation'
+ * https://leetcode.com/problems/array-reduce-transformation/
+ */
+
+/**
+ * @param {number[]} nums
+ * @param {Function} fn
+ * @param {number} init
+ * @return {number}
+ */
+function reduce(nums, fn, init) {
+  let res = init;
+  for (let i = 0; i < nums.length; ++i) {
+    res = fn(res, nums[i]);
+  }
+  return res;
+}
