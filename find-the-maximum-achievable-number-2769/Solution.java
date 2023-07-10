95dda294cf14be291ff9b844e85ef266bbdd4858
Stela Palii <stelasimonapalii@gmail.com>
2023-07-09 22:29:03 -0700

    LeetCode problem # 2769, 'Find the Maximum Achievable Number'

---
+++ find-the-maximum-achievable-number-2769/Solution.java
@@ -0,0 +1,10 @@
+/**
+ * LeetCode problem # 2769, 'Find the Maximum Achievable Number'
+ * https://leetcode.com/problems/find-the-maximum-achievable-number/
+ */
+
+class Solution {
+    public int theMaximumAchievableX(int num, int t) {
+        return num + t * 2;
+    }
+}
