7a0d0ceac7ecc183442c0d1d66e6e3800aa1b68d
Stela Palii <stelasimonapalii@gmail.com>
2023-07-30 13:53:53 -0700

    LeetCode problem # 1281, 'Subtract the Product and Sum of Digits of an Integer'

---
+++ subtract-the-product-and-sum-of-digits-of-an-integer-1281/solution.js
@@ -0,0 +1,26 @@
+/**
+ * LeetCode problem # 1281, 'Subtract the Product and Sum of Digits of an Integer'
+ * https://leetcode.com/problems/subtract-the-product-and-sum-of-digits-of-an-integer/
+ */
+
+/**
+ * @param {number} n
+ * @return {number}
+ */
+function subtractProductAndSum(n) {
+  let sum = 0;
+  let prod = 1;
+  for (const digit of digits(n)) {
+    sum += digit;
+    prod *= digit;
+  }
+  return prod - sum;
+}
+
+function* digits(num) {
+  while (num > 0) {
+    const digit = num % 10;
+    yield digit;
+    num = Math.floor(num / 10);
+  }
+}
