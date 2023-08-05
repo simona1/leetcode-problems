7a50f28be85e11bf1230cc442cacacd31af93d93
Stela Palii <stelasimonapalii@gmail.com>
2023-08-05 08:11:48 -0700

    LeetCode problem # 518, 'Coin Change II'

---
+++ coin-change-ii-518/Solution.java
@@ -0,0 +1,18 @@
+
+/**
+ * LeetCode problem # 518, 'Coin Change II'
+ * https://leetcode.com/problems/coin-change-ii/
+ */
+
+class Solution {
+    public int change(int amount, int[] coins) {
+        int[] dp = new int[amount + 1];
+        dp[0] = 1;
+        for (int i = 0; i < coins.length; ++i) {
+            for (int j = coins[i]; j <= amount; ++j) {
+                dp[j] += dp[j - coins[i]];
+            }
+        }
+        return dp[amount];
+    }
+}
