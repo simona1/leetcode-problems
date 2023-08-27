ff8de5b278e2e129a32114cea53cfd99424a1030
Stela Palii <stelasimonapalii@gmail.com>
2023-08-27 11:57:37 -0700

    LeetCode problem # 2703, 'Return Length of Arguments Passed'

---
+++ return-length-of-arguments-passed-2703/solution.js
@@ -0,0 +1,12 @@
+/**
+ * LeetCode problem # 2703, 'Return Length of Arguments Passed'
+ * https://leetcode.com/problems/return-length-of-arguments-passed/
+ */
+
+function argumentsLength(...args) {
+  return args.length;
+}
+
+/**
+ * argumentsLength(1, 2, 3); // 3
+ */
