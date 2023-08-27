42cb629b75b7371b30374a44b642f10f1ec7df1f
Stela Palii <stelasimonapalii@gmail.com>
2023-08-27 11:44:50 -0700

    Update LeetCode problem # 2626

--- array-reduce-transformation-2626/solution.js
+++ array-reduce-transformation-2626/solution.js
@@ -11,8 +11,8 @@
  */
 function reduce(nums, fn, init) {
   let res = init;
-  for (let i = 0; i < nums.length; ++i) {
-    res = fn(res, nums[i]);
+  for (const num of nums) {
+    res = fn(res, num);
   }
   return res;
 }
