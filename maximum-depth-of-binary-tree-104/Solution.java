3d00c2d03aa273631e9068e453caa28293e74ad3
Stela Palii <stelasimonapalii@gmail.com>
2023-08-27 17:10:10 -0700

    LeetCode problem # 104, iterative solution

--- maximum-depth-of-binary-tree-104/Solution.java
+++ maximum-depth-of-binary-tree-104/Solution.java
@@ -1,7 +1,10 @@
+
 /**
  * LeetCode problem # 104, 'Maximum Depth of Binary Tree'
  * https://leetcode.com/problems/maximum-depth-of-binary-tree
  */
+
+import java.util.*;
 
 class TreeNode {
     int val;
@@ -24,10 +27,25 @@ class TreeNode {
 
 class Solution {
     public int maxDepth(TreeNode root) {
+        return root == null ? 0 : Math.max(maxDepth(root.left), maxDepth(root.right)) + 1;
+    }
+
+    public int maxDepthIterative(TreeNode root) {
+        Queue<TreeNode> q = new LinkedList<>();
+        q.add(root);
         int res = 0;
-        if (root != null) {
-            return Math.max(maxDepth(root.left), maxDepth(root.right)) + 1;
+
+        while (!q.isEmpty()) {
+            int size = q.size();
+            ++res;
+            for (int i = 0; i < size; ++i) {
+                TreeNode curr = q.remove();
+                if (curr != null) {
+                    q.add(curr.left);
+                    q.add(curr.right);
+                }
+            }
         }
-        return res;
+        return res - 1;
     }
 }
