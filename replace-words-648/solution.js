b603ee84a69b1fcd39971d45b7133502f80fe05f
Stela Palii <stelasimonapalii@gmail.com>
2023-09-04 11:32:23 -0700

    LeetCode problem # 648, 'Replace Words', brute force

---
+++ replace-words-648/solution.js
@@ -0,0 +1,24 @@
+/**
+ * LeetCode problem # 648, 'Replace Words'
+ * https://leetcode.com/problems/replace-words
+ */
+
+function replaceWords(dictionary, sentence) {
+  return sentence
+    .split(" ")
+    .map((word) => findRootWord(word, dictionary))
+    .join(" ");
+}
+
+function findRootWord(word, dictionary) {
+  let res = word;
+  for (const potentialRoot of dictionary) {
+    if (!word.startsWith(potentialRoot)) {
+      continue;
+    }
+    if (potentialRoot.length < res.length) {
+      res = potentialRoot;
+    }
+  }
+  return res;
+}
