66bbf113ea8b3bca6a7d93daf4ecd2951de39a58
Stela Palii <stelasimonapalii@gmail.com>
2023-09-17 14:46:28 -0700

    LeetCode problem # 118, 'Pascal's Triangle'

---
+++ pascals-triangle-118/Solution.java
@@ -0,0 +1,29 @@
+
+/**
+ * LeetCode problem # 118, 'Pascal's Triangle'
+ * https://leetcode.com/problems/pascals-triangle
+ */
+
+import java.util.*;
+
+class Solution {
+    public List<List<Integer>> generate(int numRows) {
+        List<List<Integer>> res = new ArrayList<>();
+
+        res.add(new ArrayList<Integer>());
+        res.get(0).add(1);
+
+        for (int i = 1; i < numRows; ++i) {
+            List<Integer> row = new ArrayList<>();
+            List<Integer> prevRow = res.get(i - 1);
+
+            row.add(1);
+            for (int j = 1; j < i; ++j) {
+                row.add(prevRow.get(j - 1) + prevRow.get(j));
+            }
+            row.add(1);
+            res.add(row);
+        }
+        return res;
+    }
+}
