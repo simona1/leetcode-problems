9eb50cec9473d2fe6276fbbc56388a8711d486de
Stela Palii <stelasimonapalii@gmail.com>
2023-09-17 14:53:54 -0700

    LeetCode problem # 2804, 'Array Prototype ForEach'

---
+++ array-prototype-foreach-2804/solution.js
@@ -0,0 +1,24 @@
+/**
+ * LeetCode problem # 2804, 'Array Prototype ForEach'
+ * https://leetcode.com/problems/array-prototype-foreach
+ */
+
+/**
+ * @param {Function} callback
+ * @param {Context} context
+ */
+Array.prototype.forEach = function (callback, context) {
+  for (let i = 0; i < this.length; ++i) {
+    callback.apply(context, [this[i], i, this]);
+  }
+};
+
+/**
+ *  const arr = [1,2,3];
+ *  const callback = (val, i, arr) => arr[i] = val * 2;
+ *  const context = {"context":true};
+ *
+ *  arr.forEach(callback, context)
+ *
+ *  console.log(arr) // [2,4,6]
+ */
